3869576f9cf68c9dcda47952b80e8f1452487111
Bahar uddin <bahar.uddin@sekai-lab-bd.com>
2017-03-28 17:52:48 +0600

    Date status code completed

--- app/src/main/java/com/sample/DateStatusActivity.java
+++ app/src/main/java/com/sample/DateStatusActivity.java
@@ -1,8 +1,18 @@
 package com.sample;
 
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import com.sample.db.DatabaseHelper;
+import com.sample.models.WomanRegistration;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by User on 2/14/2017.
@@ -10,9 +20,43 @@ import butterknife.ButterKnife;
 
 public class DateStatusActivity extends AppCompatActivity {
 
+  @BindView(R.id.txt_date_due_date) TextView mDueDate;
+  @BindView(R.id.txt_date_lmp) TextView mLmpDate;
+  @BindView(R.id.txt_date_pregnant_for) TextView mPregnantDate;
+  @BindView(R.id.txt_date_fetal_age) TextView mFataAge;
+  @BindView(R.id.txt_date_remaining_time) TextView mRemainingTime;
+  private DatabaseHelper db;
+  private SimpleDateFormat dateFormat = null;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_date_status);
     ButterKnife.bind(this);
+    db = new DatabaseHelper(this);
+    dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    WomanRegistration reg = db.getRegistration(1);
+    mDueDate.setText(mDueDate.getText() + reg.getDueDate());
+    mLmpDate.setText(mLmpDate.getText() + reg.getLmpDate());
+    //Date pDate=null;
+    Date  remaining=null;
+    long diff=0,diffRemaining=0;
+
+    try {
+      diff=new Date().getTime() - dateFormat.parse(reg.getLmpDate()).getTime();
+      //pDate=new Date();
+      diffRemaining =dateFormat.parse(reg.getDueDate()).getTime()-new Date().getTime();
+
+    } catch (ParseException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    Calendar c = Calendar.getInstance();
+    c.setTime(new Date());
+    c.add(Calendar.DATE, -1 *((int)TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS)+14)); // Adding 14 days
+
+    mPregnantDate.setText(mPregnantDate.getText() +dateFormat.format(c.getTime()) );
+    mFataAge.setText(mFataAge.getText() + String.valueOf(TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS) ));
+
+    mRemainingTime.setText(mRemainingTime.getText() + String.valueOf(TimeUnit.DAYS.convert(diffRemaining, TimeUnit.MILLISECONDS) ));
   }
 }
--- app/src/main/java/com/sample/db/DatabaseHelper.java
+++ app/src/main/java/com/sample/db/DatabaseHelper.java
@@ -42,7 +42,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
   private static final String KEY_WEIGHT = "prePregnancyWeight";
   private static final String KEY_HEIGHT = "prePregnancyHeight";
 
-
   private static final String KEY_REM_ID = "id";
   private static final String KEY_REM_TITLE = "title";
   private static final String KEY_REM_MESSAGE = "message";
@@ -55,26 +54,50 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
   // Creating Tables
   @Override public void onCreate(SQLiteDatabase db) {
-    String CREATE_REG_TABLE = "CREATE TABLE " + TABLE_REGISTRATION + "("
-        + KEY_ID + " INTEGER PRIMARY KEY,"
-        + KEY_NAME + " TEXT,"
-        + KEY_DIVISION + " TEXT,"
-        + KEY_DISTRICT + " TEXT,"
-        + KEY_OPAZILA + " TEXT,"
-        + KEY_UNION + " TEXT,"
-        + KEY_MOBILE + " TEXT,"
-        + KEY_LAP + " TEXT,"
-        + KEY_DUE + " TEXT,"
-        + KEY_CYCLE + " INTEGER,"
-        + KEY_OVULATION + " INTEGER,"
-        + KEY_WEIGHT + " REAL,"
-        + KEY_HEIGHT + " INTEGER" + ")";
-    String CREATE_REMINDER_TABLE = "CREATE TABLE " + TABLE_REMINDER + "("
-        + KEY_REM_ID + " INTEGER PRIMARY KEY,"
-        + KEY_REM_TITLE + " TEXT,"
-        + KEY_REM_MESSAGE + " TEXT,"
-        + KEY_REM_DATE + " TEXT,"
-        + KEY_REM_TIME + " TEXT" + ")";
+    String CREATE_REG_TABLE = "CREATE TABLE "
+        + TABLE_REGISTRATION
+        + "("
+        + KEY_ID
+        + " INTEGER PRIMARY KEY,"
+        + KEY_NAME
+        + " TEXT,"
+        + KEY_DIVISION
+        + " TEXT,"
+        + KEY_DISTRICT
+        + " TEXT,"
+        + KEY_OPAZILA
+        + " TEXT,"
+        + KEY_UNION
+        + " TEXT,"
+        + KEY_MOBILE
+        + " TEXT,"
+        + KEY_LAP
+        + " TEXT,"
+        + KEY_DUE
+        + " TEXT,"
+        + KEY_CYCLE
+        + " INTEGER,"
+        + KEY_OVULATION
+        + " INTEGER,"
+        + KEY_WEIGHT
+        + " REAL,"
+        + KEY_HEIGHT
+        + " INTEGER"
+        + ")";
+    String CREATE_REMINDER_TABLE = "CREATE TABLE "
+        + TABLE_REMINDER
+        + "("
+        + KEY_REM_ID
+        + " INTEGER PRIMARY KEY,"
+        + KEY_REM_TITLE
+        + " TEXT,"
+        + KEY_REM_MESSAGE
+        + " TEXT,"
+        + KEY_REM_DATE
+        + " TEXT,"
+        + KEY_REM_TIME
+        + " TEXT"
+        + ")";
     db.execSQL(CREATE_REG_TABLE);
     db.execSQL(CREATE_REMINDER_TABLE);
   }
@@ -87,7 +110,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // Create tables again
     onCreate(db);
   }
-  public void addWoman(WomanRegistration womanRegistration){
+
+  public void addWoman(WomanRegistration womanRegistration) {
     SQLiteDatabase db = this.getWritableDatabase();
 
     ContentValues values = new ContentValues();
@@ -108,16 +132,43 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     db.insert(TABLE_REGISTRATION, null, values);
     db.close();
   }
+
   public int getRegistrationCount() {
     String countQuery = "SELECT  * FROM " + TABLE_REGISTRATION;
     SQLiteDatabase db = this.getReadableDatabase();
     Cursor cursor = db.rawQuery(countQuery, null);
-    int count=cursor.getCount();
+    int count = cursor.getCount();
     cursor.close();
     // return count
     return count;
   }
-  public void addReminder(Reminder reminder){
+
+  public WomanRegistration getRegistration(int id) {
+    String countQuery = "SELECT  * FROM " + TABLE_REGISTRATION ;
+    SQLiteDatabase db = this.getReadableDatabase();
+    Cursor cursor = db.rawQuery(countQuery, null);
+    WomanRegistration registration = new WomanRegistration();
+    if (cursor.moveToFirst()) {
+      registration.setId(Integer.parseInt(cursor.getString(0)));
+      registration.setFullName(cursor.getString(1));
+      registration.setDivision(cursor.getString(2));
+      registration.setDistrict(cursor.getString(3));
+      registration.setOpazila(cursor.getString(4));
+      registration.setUnion(cursor.getString(5));
+      registration.setMobile(cursor.getString(6));
+      registration.setLmpDate(cursor.getString(7));
+      registration.setDueDate(cursor.getString(8));
+      registration.setCycleDays(Integer.parseInt(cursor.getString(9)));
+      registration.setOvulationDays(Integer.parseInt(cursor.getString(10)));
+      registration.setPrePregnancyWeight(Float.parseFloat(cursor.getString(11)));
+      registration.setPrePregnancyHeight(Integer.parseInt(cursor.getString(12)));
+    }
+    cursor.close();
+
+    return registration;
+  }
+
+  public void addReminder(Reminder reminder) {
     SQLiteDatabase db = this.getWritableDatabase();
 
     ContentValues values = new ContentValues();
@@ -126,25 +177,27 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     values.put(KEY_REM_MESSAGE, reminder.getMessage());
     values.put(KEY_REM_DATE, reminder.getDate());
     values.put(KEY_REM_TIME, reminder.getTime());
-       // Inserting Row
+    // Inserting Row
     db.insert(TABLE_REMINDER, null, values);
     db.close();
   }
+
   public int getReminderCount() {
     String countQuery = "SELECT  * FROM " + TABLE_REMINDER;
     SQLiteDatabase db = this.getReadableDatabase();
     Cursor cursor = db.rawQuery(countQuery, null);
-    int count=cursor.getCount();
+    int count = cursor.getCount();
     cursor.close();
     // return count
     return count;
   }
+
   public void deleteReminder(int id) {
     SQLiteDatabase db = this.getWritableDatabase();
-    db.delete(TABLE_REMINDER, KEY_ID + " = ?",
-        new String[] { String.valueOf(id) });
+    db.delete(TABLE_REMINDER, KEY_ID + " = ?", new String[] { String.valueOf(id) });
     db.close();
   }
+
   public List<Reminder> getAllReminders() {
     List<Reminder> contactList = new ArrayList<>();
     // Select All Query
@@ -156,7 +209,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // looping through all rows and adding to list
     if (cursor.moveToFirst()) {
       do {
-        Reminder reminder=new Reminder();
+        Reminder reminder = new Reminder();
         reminder.setId(Integer.parseInt(cursor.getString(0)));
         reminder.setTitle(cursor.getString(1));
         reminder.setMessage(cursor.getString(2));
