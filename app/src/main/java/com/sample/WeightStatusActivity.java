a13ec77e8d730b6e06179e410846091a9777aa87
Bahar uddin <bahar.uddin@sekai-lab-bd.com>
2017-03-29 11:30:57 +0600

    weight status done

--- app/src/main/java/com/sample/WeightStatusActivity.java
+++ app/src/main/java/com/sample/WeightStatusActivity.java
@@ -2,7 +2,18 @@ package com.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import com.sample.db.DatabaseHelper;
+import com.sample.models.WomanRegistration;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by User on 2/14/2017.
@@ -10,10 +21,48 @@ import butterknife.ButterKnife;
 
 public class WeightStatusActivity extends AppCompatActivity {
 
+  @BindView(R.id.txt_weight_input) EditText mWeightInput;
+  @BindView(R.id.txt_weight_status) TextView mWeightStatus;
+  private DatabaseHelper db;
+  private WomanRegistration reg = null;
+  private SimpleDateFormat dateFormat = null;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_weight_status);
     ButterKnife.bind(this);
+    db = new DatabaseHelper(this);
+    dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    reg = db.getRegistration(1);
+  }
+
+  @OnClick(R.id.btn_weight_calculate) public void calculateWeight() {
+    if (mWeightInput.getText().toString().equals("")) {
+      Toast.makeText(this, "Enter Weight", Toast.LENGTH_LONG).show();
+    } else {
+
+      long diff=0,diffRemaining=0;
+
+      try {
+        diff=new Date().getTime() - dateFormat.parse(reg.getLmpDate()).getTime();
+        long days=TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+        int week=(int)days/7;
+        //now assume from weight table , weight range  will increase 2 kg after every five weeks
+        float weight=reg.getPrePregnancyWeight()+ week/5 +2;
+        float currentWeight=Float.parseFloat(mWeightInput.getText().toString());
+        if(weight>currentWeight){
+          mWeightStatus.setText("You are over weight for "+ (currentWeight-weight) +" Kg");
+        }else {
+          mWeightStatus.setText("You are under weight for "+ (weight- currentWeight) +" Kg");
+        }
+
+      } catch (ParseException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
   }
 }
+
+
 
