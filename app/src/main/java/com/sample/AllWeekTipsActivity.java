b8909a2a45acb19ae139bf01565333b9ed3b429b
Bahar uddin <bahar.uddin@sekai-lab-bd.com>
2017-03-30 11:55:33 +0600

    all week tips done

--- app/src/main/java/com/sample/AllWeekTipsActivity.java
+++ app/src/main/java/com/sample/AllWeekTipsActivity.java
@@ -2,6 +2,8 @@ package com.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -9,10 +11,11 @@ import butterknife.ButterKnife;
  */
 
 public class AllWeekTipsActivity extends AppCompatActivity {
-
+  @BindView(R.id.webview) WebView mWebView;
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_all_week_tips);
     ButterKnife.bind(this);
+    mWebView.loadUrl("file:///android_res/raw/allweektips.htm");
   }
 }
